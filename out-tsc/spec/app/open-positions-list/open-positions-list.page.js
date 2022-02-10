cb48ecb537051fd9a8490d1ad822c0ba198dbbca
Ronald Twitty <btwitty187@gmail.com>
2022-02-10 13:56:07 -0600

    turned off store fixer

--- out-tsc/spec/app/open-positions-list/open-positions-list.page.js
+++ out-tsc/spec/app/open-positions-list/open-positions-list.page.js
@@ -39,25 +39,50 @@ let OpenPositionsListPage = class OpenPositionsListPage {
         });
   
     }*/
-    getJobsByStore(storeId) {
-        //TODo add another where clause to get only open positions
-        console.log('store id in query', storeId);
-        this.firestore.collection('jobs', ref => ref.where('storeId', '==', storeId)).get()
-            .subscribe(jobs => {
-            this.jobs = [];
-            if (jobs.docs.length === 0) {
-                console.log('no jobs with that store', storeId);
-            }
-            else {
-                jobs.forEach(job => {
-                    const j = job.data();
-                    const positionId = job.id;
-                    this.jobs.push({ id: positionId, position: j });
-                    console.log(this.jobs, 'id', positionId);
-                    this.dataSource = new MatTableDataSource(this.jobs);
-                });
+    fixStoreId() {
+        const jobs = this.dbHelper.collectionWithIds$('jobs');
+        jobs.forEach(j => {
+            for (let i = 0; i < j.length; i++) {
+                const id = j[i].id;
+                const storeId = j[i].storeId;
+                const storeIdString = storeId.toString();
+                console.log('getting all jobs with Ids', j[i].storeId);
+                if (typeof storeId !== 'string') {
+                    this.firestore.doc(`jobs/${id}`).update({
+                        storeId: storeIdString
+                    }).then(res => {
+                        console.log('updated store id', id);
+                    });
+                }
             }
         });
+    }
+    getJobsByStore(storeId) {
+        this.fixStoreId();
+        if (typeof storeId === 'string') {
+            console.log('store Id is a string');
+            this.firestore.collection('jobs', ref => ref.where('storeId', '==', storeId)).get()
+                .subscribe(jobs => {
+                this.jobs = [];
+                if (jobs.docs.length === 0) {
+                    console.log('no jobs with that store', storeId);
+                }
+                else {
+                    jobs.forEach(job => {
+                        const j = job.data();
+                        const positionId = job.id;
+                        this.jobs.push({ id: positionId, position: j });
+                        console.log(this.jobs, 'id', positionId);
+                        this.dataSource = new MatTableDataSource(this.jobs);
+                    });
+                }
+            });
+        }
+        else {
+            console.log('what is store id', storeId);
+        }
+        //TODo add another where clause to get only open positions
+        console.log('store id in query', storeId);
     }
     openPositionDetails(positionId) {
         console.log('position Id', positionId);
